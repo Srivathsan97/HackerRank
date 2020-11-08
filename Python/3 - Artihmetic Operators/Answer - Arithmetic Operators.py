c86e624f6d29d6a802f38a637c3e0d140849863b
Srivathsan97 <47278326+Srivathsan97@users.noreply.github.com>
2020-11-09 01:29:52 +0530

    Added Python Arithmetic problem's solution

---
+++ Python/3 - Artihmetic Operators/Answer - Arithmetic Operators.py
@@ -0,0 +1,48 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    print(a + b)
+    
+    print(a - b)
+    
+    print(a * b)
+
+"""
+Arithmetic operators are the operators used to perform basic mathematical operations, like multiplication , addition etc., on the intergers/float variables
+Apart from arithmetic operators, there are other operations like comparison operators, assignment operators etc.
+
+NOTE: +(addition) and *(multiplication) operators can be used with strings as concatenators
+
+Example 1:
+a = 9
+b = 3
+print(a + b) 
+
+Example 2:
+a = 90
+b = 30
+print(a - b)
+
+Example 3:
+a = 2
+b = 5
+print(a * b)
+
+Example 4:
+print(float(10%9*1+2-5/3))
+print(10%9*1+2-5/3)
+print(int(10%9*1+2-5/3))
+
+Example 5:
+print("Hi " + "Human Being")
+
+Example 6:
+print("Hi " * 5, end = "\nPrinted Hi 5 times")
+
+
+Resources:
+1. https://www.tutorialspoint.com/python/python_basic_operators.htm
+2. https://www.geeksforgeeks.org/python-operators/
+3. https://www.learnpython.org/en/Basic_Operators
+"""
