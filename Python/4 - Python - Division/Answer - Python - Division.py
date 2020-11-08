6571d5b57333f0d3756070fed311e5ea35cdeae8
Srivathsan97 <47278326+Srivathsan97@users.noreply.github.com>
2020-11-09 01:45:59 +0530

    Added Python Division problem's solution

---
+++ Python/4 - Python - Division/Answer - Python - Division.py
@@ -0,0 +1,34 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    print(a//b)
+    
+    print(a/b)
+
+
+"""
+Division operator is also part of the Arithmetic operators.
+// - Returns the value in integer
+/  - Returns the value in float
+
+Example 1:
+print(4//2)
+print(4/2)
+
+Example 2:
+print(5//2)
+print(5/2)
+
+Example 3:
+print(5 // 2 / 2)
+print(5 / 2 // 2)
+print(5 / 2 / 2)
+print(5 // 2 // 2)
+
+Resources:
+1. https://www.tutorialspoint.com/python/python_basic_operators.htm
+2. https://www.geeksforgeeks.org/python-operators/
+3. https://www.learnpython.org/en/Basic_Operators
+
+"""
