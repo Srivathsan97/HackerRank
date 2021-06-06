bc766d3f8555457c0891823cc849f06c5cefffa3
Srivathsan Vijayaraghavan <47278326+Srivathsan97@users.noreply.github.com>
2021-06-06 19:09:53 +0530

    Added Python Integers come in all Sizes solution

---
+++ Python/18 - Integers Come in all Sizes/Answer - Integers come in all Sizes.py
@@ -0,0 +1,14 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+#print((pow(a,b) + pow(c,d)))
+print((a ** b) + (c ** d))
+
+
+
+"""
+This snippet is to showcase that Python doesn't have any restrictions with the length/size of the output like that in C++ where it is necessary to mention int, double, long int etc.
+"""
